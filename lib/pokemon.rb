5e6414fea1030a06a9ee9946e89d223726524819
Andrew Davisson <akdavisson4@gmail.com>
2019-06-22 19:03:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,11 +2,7 @@ class Pokemon
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS pokemon (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        type TEXT
-      )
+      
     SQL
     
     DB[:conn].execute(sql)
